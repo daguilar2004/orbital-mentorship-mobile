9757b68b058be043eeb5673dfdf9176952f8af4f
AR0902 <abdurrahmanbaig0902@gmail.com>
2026-04-15 21:13:51 -0500

    qf

--- components/accountability/AccountabilityScreen.tsx
+++ components/accountability/AccountabilityScreen.tsx
@@ -101,7 +101,6 @@ export default function AccountabilityScreen(props: Props) {
     saveEditedHabit,
     cancelHabitForm,
     startEditHabit,
-    removeHabit,
     toggleHabitCompletion,
     selectHabit,
     clearSelectedHabit,
